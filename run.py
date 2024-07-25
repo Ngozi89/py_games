67ddc65d5903e46b9e909a5c19e02fe2b4472db1
Ngozi Omenka <ngozi.madu@live.se>
2024-07-25 07:36:43 +0000

    Import module, create welcome message, game rules and name input

--- run.py
+++ run.py
@@ -1,3 +1,34 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Import the module
+import random
+
+
+#Welcome message, game rules and player name
+print("Welcome to game world")
+
+player = input("Do you want to play Rock, Paper, Scissors game? ")
+
+if player.lower() != "yes":
+    quit()
+else:
+    print("Here is the rules for the game ")  
+    print(" . There are 5 choices to chose (Fire, Wave, Fish, Boat, Grass)")
+    print()
+    print(" . You chose one and computer chose one")
+    print()
+    print(" . If your choice is same as the computer, you lose.")
+    print()
+    print(" . If your choice beats computer chose, you wine if not you lose")
+    print()
+
+player = input(" . If you are ready, type 'ok' ")
+
+if player.lower() != "ok":
+    quit()
+else:
+    print("Okay, lets' go :)")
+print()   
+# Let player enter name
+name = input("Enter your name ").capitalize()
+"Name: "
+print("Hi,", str(name) + "!")
+#End player name
